c11cc79bf2a7e3f4b1c6962f52d8f0e2eb62425a
artem.korotenko <artem.korotenko@betterme.world>
2026-03-01 14:57:55 +0200

    initial setup

---
+++ Assets/_Root/Scripts/Cursor.cs
@@ -0,0 +1,26 @@
+namespace Ai4Gamedev.Steerings
+{
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+
+    public class Cursor : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera camera;
+
+        private void Awake()
+        {
+            UnityEngine.Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            var ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            var plane = new Plane(Vector3.up, Vector3.zero);
+            if (plane.Raycast(ray, out float distance))
+            {
+                transform.position = ray.GetPoint(distance);
+            }
+        }
+    }
+}
