937cb1a1333b41cfbd3786e1b4d3c405c7bf914e
artem.korotenko <artem.korotenko@betterme.world>
2026-03-02 09:41:41 +0200

    Wander + simple Avoid edges

---
+++ Assets/_Root/Scripts/Behaviours/AvoidEdges.cs
@@ -0,0 +1,39 @@
+namespace Ai4Gamedev.Steerings.Behaviours
+{
+    using UnityEngine;
+
+    public class AvoidEdges : DesiredVelocityProvider
+    {
+        [SerializeField]
+        private Camera camera;
+        
+        private float edge = 0.05f;
+        
+        public override Vector3 GetDesiredVelocity()
+        {
+            var maxSpeed = Vehicle.VelocityLimit;
+            var v = Vehicle.Velocity;
+            var point = camera.WorldToViewportPoint(transform.position);
+
+            if (point.x > 1 - edge)
+            {
+                return new Vector3(-maxSpeed, 0, 0);
+                
+            }
+            if (point.x < edge)
+            {
+                return new Vector3(maxSpeed, 0, 0);
+            }
+            if (point.y > 1 - edge)
+            {
+                return new Vector3(0, 0, -maxSpeed);
+            }
+            if (point.y < edge)
+            {
+                return new Vector3(0, 0, maxSpeed);
+            }
+
+            return v;
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Behaviours/Wander.cs
@@ -0,0 +1,35 @@
+namespace Ai4Gamedev.Steerings.Behaviours
+{
+    using UnityEngine;
+
+    public class Wander : DesiredVelocityProvider
+    {
+        [SerializeField, Range(0.5f, 5)]
+        private float circleDistance = 1;
+        
+        [SerializeField, Range(0.5f, 5)]
+        private float circleRadius = 2;
+        
+        [SerializeField, Range(1, 80)]
+        private int angleChangeStep = 15;
+
+        private int angle = 0;
+        
+        public override Vector3 GetDesiredVelocity()
+        {
+            var rnd = Random.value;
+            if (rnd < 0.2)
+            {
+                angle+= angleChangeStep;
+            } else if (rnd > 0.8)
+            {
+                angle-= angleChangeStep;
+            }
+            
+            var futurePos = Vehicle.transform.position + Vehicle.Velocity.normalized * circleDistance;
+            var vector = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad),0, Mathf.Sin(angle * Mathf.Deg2Rad)) * circleRadius;
+
+            return (futurePos + vector - transform.position).normalized * Vehicle.VelocityLimit;
+        }
+    }
+}
